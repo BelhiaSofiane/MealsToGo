426481d963160c7a654057f885e23459340ce490
sofiane belhia <sofianebelhia150@gmail.com>
2025-10-03 15:16:02 +0100

    -added new component for restaurant

--- src/features/restaurants/components/restaurant-info.js
+++ src/features/restaurants/components/restaurant-info.js
@@ -1,8 +1,15 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React from "react";
+import { Text } from "react-native";
 
-export const RestaurantInfo = () => {
-    return (
-        <Text>RestaurantInfo</Text>
-    )
-}
+export const RestaurantInfo = ({ restaurant = {} }) => {
+  const {
+    name = 'Some Restaurant',
+    icon ,
+    photos = ['https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/restaurant-71.png'],
+    address = '100 some random street',
+    isOpenNow = true,
+    rating =  4,
+    isClosedTemporarily,
+  } = restaurant;
+  return <Text>{name}</Text>;
+};
