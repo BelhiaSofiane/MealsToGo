139fba69c18b3286be0373cbb1ecb6b4ad693acf
Sofiane Belhia <Sofianebelhia150@gmail.com>
2025-10-03 21:34:15 +0200

    -updated restaurant.screen to use styled-components

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,49 +1,49 @@
 import * as React from "react";
+import styled from "styled-components/native";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import {
   StatusBar,
-  StyleSheet,
   View,
   SafeAreaView,
   Platform,
 } from "react-native";
 import { Searchbar } from "react-native-paper";
 import { RestaurantInfoCard } from "../components/restaurant-info-card";
+const isAndroid = Platform.OS === "android";
 
-const isAndroind = Platform.OS === "android";
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  margin-top: ${isAndroid ? `${StatusBar.currentHeight}px` : "0px"};
+`;
+const SearchContainer = styled.View`
+  padding: 16px;
+`;
+
+const RestaurantListContainer = styled.View`
+  flex: 1;
+  padding: 16px;
+  background-color: blue;
+`;
+
 export const RestaurantsScreen = () => {
   const [searchQuery, setSearchQuery] = React.useState("");
 
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
+      <SafeArea>
+        <SearchContainer>
           <Searchbar
             placeholder="Search"
             onChangeText={setSearchQuery}
             value={searchQuery}
           />
-        </View>
-        <View style={styles.list}>
+        </SearchContainer>
+        <RestaurantListContainer>
           <RestaurantInfoCard />
-        </View>
-      </SafeAreaView>
+        </RestaurantListContainer>
+      </SafeArea>
       <ExpoStatusBar style="auto" />
     </>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: isAndroind ? StatusBar.currentHeight : 0,
-  },
-  search: {
-    padding: 16,
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: 'blue',
-  },
-});
